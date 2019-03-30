5c812ea064f42544257f813214445454fe0ab8f2
Amirul Sunesara <amirul.sunesara@dal.ca>
2019-03-30 01:07:22 -0300

    Fixed merge issues

--- app/src/main/java/com/example/locusreminder/display/ViewReminders.java
+++ app/src/main/java/com/example/locusreminder/display/ViewReminders.java
@@ -1,15 +1,24 @@
 package com.example.locusreminder.display;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -22,8 +31,10 @@ import android.widget.TextView;
 
 import com.example.locusreminder.R;
 import com.example.locusreminder.adapter.ReminderAdapter;
+import com.example.locusreminder.db.DBManager;
 import com.example.locusreminder.db.ReminderData;
 import com.example.locusreminder.model.ReminderModel;
+import com.example.locusreminder.service.LocationService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +44,8 @@ public class ViewReminders extends AppCompatActivity
 
     private Activity activity;
     FloatingActionButton fab;
+    DBManager dbManager;
+    private BroadcastReceiver broadcastReceiver;
 
     public FloatingActionButton getFloatingActionButton() {
         return fab;
@@ -46,7 +59,7 @@ public class ViewReminders extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
        setTitle(R.string.view_reminders);
-
+        dbManager = new DBManager(this);
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,9 +87,36 @@ public class ViewReminders extends AppCompatActivity
         TextView tvPhoneName = headerView.findViewById(R.id.tvPhoneName);
         tvPhoneName.setText(Build.MODEL);
 
+        reqPermission();
+
+        Intent i =new Intent(getApplicationContext(), LocationService.class);
+        startService(i);
+
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
 
+    }
+
+    private boolean reqPermission() {
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},100);
+
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100){
+            if( grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+            }else {
+                reqPermission();
+            }
+        }
     }
 
     @Override
@@ -118,30 +158,96 @@ public class ViewReminders extends AppCompatActivity
         return true;
     }
 
-    public void createNotification()
+    public void createNotification(int id,ReminderData reminderData)
     {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.putExtra("Latitude",reminderData.getLatitude());
+        intent.putExtra("Longitude",reminderData.getLongitude());
+        intent.putExtra("selected_pace",reminderData.getLocation());
+        intent.putExtra("title",reminderData.getTitle());
+        intent.putExtra("NoteText",reminderData.getNote());
+        intent.putExtra("mode","update");
+        intent.putExtra("id",reminderData.getId());
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "Channel")
-                .setSmallIcon(R.drawable.ic_home_black_24dp)
-                .setContentTitle("My notification")
-                .setContentText("Much longer text that cannot fit one line...")
+                .setSmallIcon(R.drawable.ic_add_alert_black_24dp)
+                .setContentTitle("Reminder alert: "+reminderData.getTitle())
+                .setContentText(reminderData.getNote())
                 .setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText("Much longer text that cannot fit one line..."))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                        .bigText(reminderData.getNote()))
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_MAX);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             String channelId = "Channel";
             NotificationChannel channel = new NotificationChannel(channelId,
                     "description",
-                    NotificationManager.IMPORTANCE_DEFAULT);
+                    NotificationManager.IMPORTANCE_HIGH);
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
 
         }
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(0, builder.build());
+        notificationManager.notify(id, builder.build());
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(broadcastReceiver == null){
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+
+                    Double longitude1 = Double.parseDouble(intent.getExtras().get("longitudeCoordinate").toString());
+                    Double latitude2 = Double.parseDouble(intent.getExtras().get("latitudeCoordinate").toString());
+                    Location location1= new Location("");
+                    location1.setLatitude(latitude2);
+                    location1.setLongitude(longitude1);
+                    List<ReminderData> lstReminder = dbManager.getReminderData();
+
+                    int i=0;
+                    for (ReminderData rd: lstReminder) {
+
+                        if(rd.getIsDeleted().equals("0")){
+                            Location location2 = new Location("");
+                            location2.setLatitude(Double.parseDouble(rd.getLatitude()));
+                            location2.setLongitude(Double.parseDouble(rd.getLongitude()));
+
+                            float distance = location1.distanceTo(location2);
+
+                            if(distance <= 100)
+                            {
+                                createNotification(i,rd);
+                            }
+
+                        }
+                        i++;
+                    }
+
+
+
+
+                }
+            };
+        }
+        registerReceiver(broadcastReceiver,new IntentFilter("locationCoordinates"));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(broadcastReceiver != null){
+            unregisterReceiver(broadcastReceiver);
+        }
+    }
+
+
 }
 
 
