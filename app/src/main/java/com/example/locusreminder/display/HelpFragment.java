b9daa06a54c482f8538309d6000d41de338dba17
Ajith <aj788769@dal.ca>
2019-03-31 15:22:30 -0300

    help and documentation changes

--- app/src/main/java/com/example/locusreminder/display/HelpFragment.java
+++ app/src/main/java/com/example/locusreminder/display/HelpFragment.java
@@ -27,6 +27,15 @@ public class HelpFragment extends Fragment {
         final TextView mItemDescription1 = (TextView) view.findViewById(R.id.item_description1);
         final ImageView mDescriptionImg1=(ImageView) view.findViewById(R.id.item_description_img1);
         final LinearLayout layout_one1=(LinearLayout)view.findViewById(R.id.item_description_layout1);
+        final TextView mItemDescription3 = (TextView) view.findViewById(R.id.item_description3);
+        final ImageView mDescriptionImg3=(ImageView) view.findViewById(R.id.item_description_img3);
+        final LinearLayout layout_one3=(LinearLayout)view.findViewById(R.id.item_description_layout3);
+        final TextView mItemDescription4 = (TextView) view.findViewById(R.id.item_description4);
+        final ImageView mDescriptionImg4=(ImageView) view.findViewById(R.id.item_description_img4);
+        final LinearLayout layout_one4=(LinearLayout)view.findViewById(R.id.item_description_layout4);
+        final TextView mItemDescription5 = (TextView) view.findViewById(R.id.item_description5);
+        final ImageView mDescriptionImg5=(ImageView) view.findViewById(R.id.item_description_img5);
+        final LinearLayout layout_one5=(LinearLayout)view.findViewById(R.id.item_description_layout5);
         layout_one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,7 +67,61 @@ public class HelpFragment extends Fragment {
                     mDescriptionImg1.setImageResource(R.drawable.ic_arrow_drop_down_black_24dp);
                 }
 
-                ObjectAnimator animation = ObjectAnimator.ofInt(mItemDescription, "maxLines", mItemDescription1.getMaxLines());
+                ObjectAnimator animation = ObjectAnimator.ofInt(mItemDescription1, "maxLines", mItemDescription1.getMaxLines());
+                animation.setDuration(200).start();
+
+            }
+        });
+        layout_one3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mItemDescription3.getVisibility() == View.INVISIBLE) {
+                    // it's collapsed - expand it
+                    mItemDescription3.setVisibility(View.VISIBLE);
+                    mDescriptionImg3.setImageResource(R.drawable.ic_arrow_drop_up_black_24dp);
+                } else {
+                    // it's expanded - collapse it
+                    mItemDescription3.setVisibility(View.INVISIBLE);
+                    mDescriptionImg3.setImageResource(R.drawable.ic_arrow_drop_down_black_24dp);
+                }
+
+                ObjectAnimator animation = ObjectAnimator.ofInt(mItemDescription3, "maxLines", mItemDescription3.getMaxLines());
+                animation.setDuration(200).start();
+
+            }
+        });
+        layout_one4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mItemDescription4.getVisibility() == View.INVISIBLE) {
+                    // it's collapsed - expand it
+                    mItemDescription4.setVisibility(View.VISIBLE);
+                    mDescriptionImg4.setImageResource(R.drawable.ic_arrow_drop_up_black_24dp);
+                } else {
+                    // it's expanded - collapse it
+                    mItemDescription4.setVisibility(View.INVISIBLE);
+                    mDescriptionImg4.setImageResource(R.drawable.ic_arrow_drop_down_black_24dp);
+                }
+
+                ObjectAnimator animation = ObjectAnimator.ofInt(mItemDescription4, "maxLines", mItemDescription4.getMaxLines());
+                animation.setDuration(200).start();
+
+            }
+        });
+        layout_one5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mItemDescription5.getVisibility() == View.INVISIBLE) {
+                    // it's collapsed - expand it
+                    mItemDescription5.setVisibility(View.VISIBLE);
+                    mDescriptionImg5.setImageResource(R.drawable.ic_arrow_drop_up_black_24dp);
+                } else {
+                    // it's expanded - collapse it
+                    mItemDescription5.setVisibility(View.INVISIBLE);
+                    mDescriptionImg5.setImageResource(R.drawable.ic_arrow_drop_down_black_24dp);
+                }
+
+                ObjectAnimator animation = ObjectAnimator.ofInt(mItemDescription5, "maxLines", mItemDescription5.getMaxLines());
                 animation.setDuration(200).start();
 
             }
